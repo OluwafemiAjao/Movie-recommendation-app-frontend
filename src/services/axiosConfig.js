c6a26b4b7454ca968e9bec219b92cca08812ea07
OluwafemiAjao <oluwafemisamuelajao@gmail.com>
2025-09-11 05:28:16 +0100

    axiosConfig.js

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 const backendApi = axios.create({
   // baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api", // later replace with Render/Heroku URL
-  baseURL: "https://movie-recommendation-app-backend-4ghi.onrender.com/api",
+  // baseURL: "https://movie-recommendation-app-backend-4ghi.onrender.com/api",
+  baseURL: "https://movie-recommendation-app-backend-4ghi.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
